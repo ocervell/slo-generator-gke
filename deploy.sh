56809558628bd56f2f8369783612ccdfc6f1841d
Olivier Cervello <ocervello@google.com>
2021-09-20 10:34:42 +0200

    Update deployment for v2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-kubectl create configmap slos --from-file slos/ -o yaml --dry-run=client | kubectl apply -f -
-kubectl create configmap config --from-file config.yaml -o yaml --dry-run=client | kubectl apply -f -
-kubectl apply -f app.yaml
+kubectl create namespace slo || echo "SLO namespace already exist."
+kubectl create -n slo configmap slos --from-file slos/ -o yaml --dry-run=client | kubectl apply -f -
+kubectl create -n slo configmap config --from-file config.yaml -o yaml --dry-run=client | kubectl apply -f -
+kubectl apply -n slo -f app.yaml
